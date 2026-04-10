bf7d71c6002820d2ad9fafdfafd17ff156e8dd83
Lbartle79 <161118476+Lbartle79@users.noreply.github.com>
2026-04-10 17:44:29 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 const stops = [
-
   {
     lat:52.859893, lng:-104.601088,
     start:"2025-07-19 05:42",
@@ -17,52 +16,5 @@ const stops = [
     start:"2025-07-19 06:38",
     end:"2025-07-19 07:06",
     duration:"28 min"
-  },
-  {
-    lat:52.859795, lng:-104.600945,
-    start:"2025-07-19 07:19",
-    end:"2025-07-19 07:31",
-    duration:"12 min"
-  },
-  {
-    lat:52.859767, lng:-104.601053,
-    start:"2025-07-19 07:34",
-    end:"2025-07-19 07:44",
-    duration:"9 min"
-  },
-  {
-    lat:52.859988, lng:-104.601015,
-    start:"2025-07-19 07:47",
-    end:"2025-07-19 08:18",
-    duration:"31 min"
-  },
-  {
-    lat:52.859835, lng:-104.600825,
-    start:"2025-07-19 08:21",
-    end:"2025-07-19 08:33",
-    duration:"12 min"
-  },
-  {
-    lat:52.859882, lng:-104.601023,
-    start:"2025-07-19 08:45",
-    end:"2025-07-19 08:52",
-    duration:"7 min"
-  },
-  {
-    lat:52.860102, lng:-104.601050,
-    start:"2025-07-19 08:58",
-    end:"2025-07-19 09:07",
-    duration:"9 min"
-  },
-  {
-    lat:52.859878, lng:-104.601088,
-    start:"2025-07-19 09:16",
-    end:"2025-07-19 09:23",
-    duration:"6 min"
   }
-
-  // ⚠️ NOTE:
-  // This is a CLEAN SAMPLE to get your popup working correctly first.
-  // I will expand this to FULL July once you confirm it's working.
-
 ];
