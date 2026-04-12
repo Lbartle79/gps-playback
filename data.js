a2caacafd1c5e4543687f4127784683198268009
Lbartle79 <161118476+Lbartle79@users.noreply.github.com>
2026-04-11 18:37:59 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,8 @@
 const stops = [
 
-  // --- Base test point ---
   { lat: 52.86, lng: -104.60 },
 
-  // --- October 30 ---
+  // --- October ---
   {
     lat: 52.860132,
     lng: -104.600817,
@@ -26,6 +25,22 @@ const stops = [
     duration: "8 minutes"
   },
 
+  // --- More October ---
+  {
+    lat: 52.859930,
+    lng: -104.600898,
+    start: "2025-10-30 20:04",
+    end: "2025-10-30 20:40",
+    duration: "36 minutes"
+  },
+  {
+    lat: 52.859617,
+    lng: -104.601100,
+    start: "2025-10-30 20:41",
+    end: "2025-10-30 20:54",
+    duration: "13 minutes"
+  },
+
   // --- July ---
   {
     lat: 52.859893,
@@ -33,15 +48,6 @@ const stops = [
     start: "2025-07-19 05:42",
     end: "2025-07-19 06:20",
     duration: "37 minutes"
-  },
-
-  // --- September ---
-  {
-    lat: 52.860050,
-    lng: -104.601100,
-    start: "2025-09-15 10:12",
-    end: "2025-09-15 10:40",
-    duration: "28 minutes"
   }
 
 ];
