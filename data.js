064ef5024c7fe43ed4c6da99ff721e07bdb02942
Lbartle79 <161118476+Lbartle79@users.noreply.github.com>
2026-04-11 18:18:33 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,4 @@
 const stops = [
-  {
-    lat: 52.86,
-    lng: -104.60,
-    start: "TEST",
-    end: "TEST",
-    duration: "TEST"
-  },
-  {
-    lat: 52.861,
-    lng: -104.601,
-    start: "TEST",
-    end: "TEST",
-    duration: "TEST"
-  }
+  { lat: 52.86, lng: -104.60 },
+  { lat: 52.861, lng: -104.601 }
 ];
