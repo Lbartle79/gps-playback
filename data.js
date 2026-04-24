74c1ff92c92888e06b2b6ac4d8d66be4313ca379
Lbartle79 <161118476+Lbartle79@users.noreply.github.com>
2026-04-23 22:39:50 -0600

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const stops = [
 
   // ================= SEPTEMBER =================
 
-  // Sept 15
+  // --- Sept 15 ---
   {
     lat: 52.859892,
     lng: -104.601142,
@@ -17,20 +17,6 @@ const stops = [
     lng: -104.601139,
     start: "2025-09-15 06:18",
     end: "2025-09-15 06:27",
-    duration: "8 minutes"
-  },
-  {
-    lat: 52.859860,
-    lng: -104.601147,
-    start: "2025-09-15 06:31",
-    end: "2025-09-15 06:37",
-    duration: "6 minutes"
-  },
-  {
-    lat: 52.859815,
-    lng: -104.601040,
-    start: "2025-09-15 06:53",
-    end: "2025-09-15 07:01",
     duration: "8 minutes"
   },
   {
@@ -48,7 +34,7 @@ const stops = [
     duration: "9 hours 23 minutes"
   },
 
-  // Sept 17–18
+  // --- Sept 17–18 ---
   {
     lat: 52.859990,
     lng: -104.601167,
@@ -78,7 +64,7 @@ const stops = [
     duration: "1 hour 26 minutes"
   },
 
-  // Sept 19–20
+  // --- Sept 19–20 ---
   {
     lat: 52.859920,
     lng: -104.601125,
@@ -94,13 +80,6 @@ const stops = [
     duration: "33 minutes"
   },
   {
-    lat: 52.863165,
-    lng: -104.600258,
-    start: "2025-09-19 16:35",
-    end: "2025-09-19 17:27",
-    duration: "52 minutes"
-  },
-  {
     lat: 52.859857,
     lng: -104.601127,
     start: "2025-09-19 20:08",
@@ -113,6 +92,80 @@ const stops = [
     start: "2025-09-19 21:46",
     end: "2025-09-20 01:30",
     duration: "3 hours 44 minutes"
+  },
+  {
+    lat: 52.859915,
+    lng: -104.601063,
+    start: "2025-09-20 02:28",
+    end: "2025-09-20 04:43",
+    duration: "2 hours 15 minutes"
+  },
+
+  // --- Sept 23 ---
+  {
+    lat: 52.859922,
+    lng: -104.601123,
+    start: "2025-09-23 06:46",
+    end: "2025-09-23 10:46",
+    duration: "4 hours"
+  },
+  {
+    lat: 52.859885,
+    lng: -104.600983,
+    start: "2025-09-23 10:47",
+    end: "2025-09-23 15:11",
+    duration: "4 hours 23 minutes"
+  },
+  {
+    lat: 52.859940,
+    lng: -104.601082,
+    start: "2025-09-23 16:49",
+    end: "2025-09-23 17:11",
+    duration: "22 minutes"
+  },
+  {
+    lat: 52.859893,
+    lng: -104.601072,
+    start: "2025-09-23 17:17",
+    end: "2025-09-23 21:25",
+    duration: "4 hours 8 minutes"
+  },
+  {
+    lat: 52.859965,
+    lng: -104.601022,
+    start: "2025-09-23 22:22",
+    end: "2025-09-23 22:42",
+    duration: "20 minutes"
+  },
+
+  // --- Sept 25 ---
+  {
+    lat: 52.859917,
+    lng: -104.601027,
+    start: "2025-09-25 00:00",
+    end: "2025-09-25 01:31",
+    duration: "1 hour 31 minutes"
+  },
+  {
+    lat: 52.859863,
+    lng: -104.600982,
+    start: "2025-09-25 05:27",
+    end: "2025-09-25 05:33",
+    duration: "6 minutes"
+  },
+  {
+    lat: 52.859858,
+    lng: -104.601193,
+    start: "2025-09-25 11:37",
+    end: "2025-09-25 19:54",
+    duration: "8 hours 17 minutes"
+  },
+  {
+    lat: 52.859943,
+    lng: -104.601002,
+    start: "2025-09-25 20:18",
+    end: "2025-09-25 21:53",
+    duration: "1 hour 35 minutes"
   }
 
 ];
